6345f92d44649ca1df733947a9c3da6bdca4abf1
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-02 23:01:32 +0300

    add /dto/users

---
+++ src/main/java/hexlet/code/dto/users/UserCreateDTO.java
@@ -0,0 +1,24 @@
+package hexlet.code.dto.users;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserCreateDTO {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "\\w{6,}")
+    private String password;
+}
---
+++ src/main/java/hexlet/code/dto/users/UserUpdateDTO.java
@@ -0,0 +1,26 @@
+package hexlet.code.dto.users;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Setter
+@Getter
+public class UserUpdateDTO {
+
+    @NotBlank
+    private JsonNullable<String> firstName;
+
+    @NotBlank
+    private JsonNullable<String> lastName;
+
+    @Email
+    private JsonNullable<String> email;
+
+    @Pattern(regexp = "\\w{6,}")
+    private JsonNullable<String> password;
+
+}
