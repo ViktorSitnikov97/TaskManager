308530afc62cb0f3676db5b8e9b7c12d9b7b24cf
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-02 23:05:20 +0300

    add DataInitializer.java

---
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -0,0 +1,37 @@
+package hexlet.code.component;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+//    @Autowired
+//    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var email = "hexlet@example.com";
+        var rawPassword = "qwerty";
+        Optional<User> firstUser = userRepository.findByEmail(email);
+        if (firstUser.isEmpty()) {
+            var userData = new User();
+            userData.setEmail(email);
+//            userData.setPassword(passwordEncoder.encode(rawPassword));
+            userData.setPasswordDigest(rawPassword);
+            userRepository.save(userData);
+        }
+    }
+}
