c42cff4dabad49cca9ab7dc4c2daea2877197739
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-02 16:25:06 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,17 +34,26 @@ dependencies {
 	compileOnly("org.projectlombok:lombok:1.18.34")
 	annotationProcessor("org.projectlombok:lombok:1.18.34")
 
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
 
+	runtimeOnly("com.h2database:h2")
+	implementation("org.postgresql:postgresql:42.7.2")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	implementation("net.datafaker:datafaker:2.4.0")
 	implementation("org.instancio:instancio-junit:5.0.1")
 
-
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
