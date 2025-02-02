98e910781bfd92394b2149f4ac28183300197da1
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-03 01:14:40 +0300

    add UserController.java

---
+++ src/main/java/hexlet/code/controller/api/UserController.java
@@ -0,0 +1,61 @@
+package hexlet.code.controller.api;
+
+
+import hexlet.code.dto.users.UserCreateDTO;
+import hexlet.code.dto.users.UserDTO;
+import hexlet.code.dto.users.UserUpdateDTO;
+import hexlet.code.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(path = "")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> index() {
+        return userService.getAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO show(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO create(@Valid @RequestBody UserCreateDTO data) {
+        return userService.create(data);
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO update(@Valid @RequestBody UserUpdateDTO data, @PathVariable Long id) {
+        return userService.update(data, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.destroy(id);
+    }
+
+
+}
