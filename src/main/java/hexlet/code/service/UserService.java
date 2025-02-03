59f13faa0a4cb048954f8b0190abd5f6b9047475
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-04 01:59:18 +0300

    update UserService.java

--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -7,18 +7,31 @@ import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.UserMapper;
 import hexlet.code.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsManager {
 
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private UserMapper mapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email '" + email + "' not found!"));
+    }
 
     public UserDTO findById(Long id) {
         var user = userRepository.findById(id)
@@ -34,6 +47,7 @@ public class UserService {
 
     public UserDTO create(UserCreateDTO data) {
         var user = mapper.map(data);
+        user.setPasswordDigest(passwordEncoder.encode(data.getPassword()));
         userRepository.save(user);
 
         return mapper.map(user);
@@ -43,6 +57,9 @@ public class UserService {
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User with id = " + id + " not found"));
         mapper.update(data, user);
+        if (!(data.getPassword() == null)) { // isPresent()
+            user.setPasswordDigest(passwordEncoder.encode(data.getPassword().get()));
+        }
         userRepository.save(user);
         return mapper.map(user);
     }
@@ -53,4 +70,33 @@ public class UserService {
         userRepository.delete(user);
     }
 
+    @Override
+    public void createUser(UserDetails userData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changePassword'");
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'userExists'");
+    }
 }
