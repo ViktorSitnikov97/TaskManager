095dae2dcd66ede47ce54f4a0709f6b68b2d7ca2
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-04 18:01:15 +0300

    add AuthenticationController.java

---
+++ src/test/java/hexlet/code/controller/api/AuthenticationControllerTest.java
@@ -0,0 +1,39 @@
+package hexlet.code.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.dto.AuthRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthenticationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    public void testIndex() throws Exception {
+        AuthRequest authRequest = new AuthRequest();
+        authRequest.setUsername("hexlet@example.com");
+        authRequest.setPassword("qwerty");
+
+        MockHttpServletRequestBuilder request = post("/api/login").with(jwt())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(authRequest));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+    }
+}
