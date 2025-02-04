561c131cafad4b8c009155f6295e2b10904a435e
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-04 18:01:41 +0300

    add UserUtils.java

---
+++ src/main/java/hexlet/code/utils/UserUtils.java
@@ -0,0 +1,36 @@
+package hexlet.code.utils;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class UserUtils {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getCurrentUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        var email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new AuthenticationCredentialsNotFoundException("Not Authorised"));
+    }
+
+    public boolean isAuthor(long id) {
+        var user = Optional.ofNullable(getCurrentUser()).orElse(new User());
+        return Objects.equals(user.getId(), id);
+    }
+    public User getTestUser() {
+        return  userRepository.findByEmail("hexlet@example.com")
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
