539f25590158312774ceb62a3681cc511d1db4a4
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-05 02:45:21 +0300

    update UserController.java

--- src/main/java/hexlet/code/controller/api/UserController.java
+++ src/main/java/hexlet/code/controller/api/UserController.java
@@ -9,6 +9,7 @@ import hexlet.code.utils.UserUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,9 +34,11 @@ public class UserController {
     private UserUtils userUtils;
 
     @GetMapping(path = "")
-    @ResponseStatus(HttpStatus.OK)
-    public List<UserDTO> index() {
-        return userService.getAll();
+    public ResponseEntity<List<UserDTO>> index() {
+        var users =  userService.getAll();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(users.size()))
+                .body(users);
     }
 
     @GetMapping(path = "/{id}")
