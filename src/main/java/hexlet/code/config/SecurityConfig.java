385ac64bef74f51e91738034570e76043474c32a
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-05 02:49:29 +0300

    add Frontend-part of project TaskManager

--- src/main/java/hexlet/code/config/SecurityConfig.java
+++ src/main/java/hexlet/code/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login").permitAll()
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/index.html").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer((rs) -> rs.jwt((jwt) -> jwt.decoder(jwtDecoder)))
