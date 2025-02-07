c80ddc3bb626fa118cd050a7bada54eb61478403
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-07 03:22:18 +0300

    add Task.java

---
+++ src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,51 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tasks")
+public class Task implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Integer index;
+
+    @NotBlank
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @NotBlank
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "task_status_id", nullable = false)
+    private TaskStatus taskStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+    @CreatedDate
+    private LocalDate createdAt;
+}
