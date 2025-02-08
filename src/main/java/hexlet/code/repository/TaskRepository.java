7bdce52dd9bfe4aa03a94a87c2d60e2f176d23ac
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-08 03:58:25 +0300

    update TaskStatusRepository.java and TaskRepository.java

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -2,10 +2,11 @@ package hexlet.code.repository;
 
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findByNameAndIndex(String name, Integer index);
 }
