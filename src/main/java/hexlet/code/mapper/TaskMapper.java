e195b683c4cd74ff62f90bf0a03d75e9c84f8caf
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-08 03:56:56 +0300

    update TaskMapper.java

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -4,8 +4,10 @@ import hexlet.code.dto.tasks.TaskCreateDTO;
 import hexlet.code.dto.tasks.TaskDTO;
 import hexlet.code.dto.tasks.TaskUpdateDTO;
 import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.Label;
 import hexlet.code.model.Task;
 import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -15,6 +17,10 @@ import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
@@ -27,29 +33,36 @@ public abstract class TaskMapper {
     @Autowired
     private TaskStatusRepository taskStatusRepository;
 
+    @Autowired
+    private LabelRepository labelRepository;
+
     @Mapping(target = "name", source = "title")
-    @Mapping(target = "assignee.id", source = "assigneeId")
+    @Mapping(target = "assignee", source = "assigneeId")
     @Mapping(target = "description", source = "content")
     @Mapping(target = "taskStatus", source = "status", qualifiedByName = "getTaskStatusByStatusSlug")
+    @Mapping(target = "labels", source = "taskLabelsIds", qualifiedByName = "getLabelsBySetIds")
     public abstract Task map(TaskCreateDTO data);
 
     @Mapping(target = "assigneeId", source = "assignee.id")
     @Mapping(target = "content", source = "description")
     @Mapping(target = "status", source = "taskStatus.slug")
     @Mapping(target = "title", source = "name")
+    @Mapping(target = "taskLabelsIds", source = "labels", qualifiedByName = "getIdsBySetLabels")
     public abstract TaskDTO map(Task model);
 
     @Mapping(target = "name", source = "title")
     @Mapping(target = "assignee", source = "assigneeId")
     @Mapping(target = "description", source = "content")
     @Mapping(target = "taskStatus", source = "status", qualifiedByName = "getTaskStatusByStatusSlug")
-    public abstract Task update(TaskUpdateDTO data, @MappingTarget Task model);
+    @Mapping(target = "labels", source = "taskLabelsIds", qualifiedByName = "getLabelsBySetIds")
+    public abstract void update(TaskUpdateDTO data, @MappingTarget Task model);
 
 
     @Mapping(target = "assigneeId", source = "assignee.id")
     @Mapping(target = "content", source = "description")
     @Mapping(target = "status", source = "taskStatus.slug")
     @Mapping(target = "title", source = "name")
+    @Mapping(target = "taskLabelsIds", source = "labels", qualifiedByName = "getIdsBySetLabels")
     public abstract TaskCreateDTO mapToCreateDTO(Task model);
 
 
@@ -61,5 +74,24 @@ public abstract class TaskMapper {
                 );
     }
 
+    @Named("getLabelsBySetIds")
+    public Set<Label> getLabelsBSetIds(Set<Long> ids) {
+        return ids == null
+                ?
+                new HashSet<>()
+                :
+                labelRepository.findByIdIn(ids);
+    }
+
+    @Named("getIdsBySetLabels")
+    public Set<Long> getIdsBySetLabels(Set<Label> labels) {
+        return labels == null
+                ?
+                new HashSet<>()
+                :
+                labels.stream()
+                        .map(Label::getId)
+                        .collect(Collectors.toSet());
+    }
 
 }
