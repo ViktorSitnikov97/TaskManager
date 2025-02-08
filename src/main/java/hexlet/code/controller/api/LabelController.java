ef8fc9a5535e5a999a3a03ffce86eba443c995c0
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-08 17:33:19 +0300

    add LabelController.java

---
+++ src/main/java/hexlet/code/controller/api/LabelController.java
@@ -0,0 +1,61 @@
+package hexlet.code.controller.api;
+
+import hexlet.code.dto.labels.LabelCreateDTO;
+import hexlet.code.dto.labels.LabelDTO;
+import hexlet.code.dto.labels.LabelUpdateDTO;
+import hexlet.code.service.LabelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/labels")
+public class LabelController {
+
+    @Autowired
+    private LabelService labelService;
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<LabelDTO>> index() {
+        var labels = labelService.getAll();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(labels.size()))
+                .body(labels);
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LabelDTO show(@PathVariable Long id) {
+        return labelService.findById(id);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public LabelDTO create(@RequestBody LabelCreateDTO data) {
+        return labelService.create(data);
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LabelDTO update(@RequestBody LabelUpdateDTO data, @PathVariable Long id) {
+        return labelService.update(data, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        labelService.destroy(id);
+    }
+}
