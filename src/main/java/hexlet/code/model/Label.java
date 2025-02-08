c02262b32c5691f8d891cd29890ac200b3ad18b4
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-02-08 03:49:01 +0300

    add Label.java

---
+++ src/main/java/hexlet/code/model/Label.java
@@ -0,0 +1,46 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "labels")
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Label {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    @Size(min = 3, max = 1000)
+    private String name;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @ManyToMany(mappedBy = "labels")
+    private Set<Task> tasks = new HashSet<>();
+}
